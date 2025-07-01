a2bff33b7029d2bab602ffdf3d516075310aead7
Juan Jose Estrada Arana <erp.sheet@gmail.com>
2025-06-30 21:29:21 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,13 @@
-// auth.js - VERSIÓN DE DIAGNÓSTICO 1
+// auth.js - VERSIÓN 2: Añadiendo la conexión a Supabase
 
+// --- Configuración de Supabase ---
+const supabaseUrl = 'https://vqdgrzrxnqrgkafuwppy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZGdyenJ4bnFyZ2thZnV3cHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDYxNjgsImV4cCI6MjA2NTU4MjE2OH0.FPcoDcsWrS-y9LwiY0mgHfjA3C4svP4lGP11vNygktQ';
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase Client creado exitosamente.");
+
+// Mantenemos nuestra función de prueba por ahora
 function handleLogout() {
-  // Por ahora, solo mostraremos una alerta para probar.
-  alert("¡ÉXITO! La función handleLogout() funciona.");
-  // Después, aquí pondremos la lógica de Supabase para cerrar sesión.
+  alert("¡ÉXITO! La función handleLogout() sigue funcionando con Supabase conectado.");
 }
-
-console.log("auth.js (versión simple) cargado correctamente.");
