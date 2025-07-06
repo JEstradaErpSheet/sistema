ccbec1bf76c81b6652cbab18a4a1463e770e9f97
JEstradaErpSheet <erp.sheet@gmail.com>
2025-07-06 13:19:17 -0600

    FIX: Corregir Project URL en cliente Supabase

--- supabase-client.js
+++ supabase-client.js
@@ -2,7 +2,7 @@
 // Este archivo contiene la configuración central de Supabase y las funciones de ayuda.
 // Es seguro incluirlo en TODAS las páginas que necesiten interactuar con Supabase.
 
-const supabaseUrl = 'https://vqdgrznxqrgkafuwppy.supabase.co';
+const supabaseUrl = 'https://vqdgrzrxnqrgkafuwppy.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZGdyenJ4bnFyZ2thZnV3cHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NDg0MDcsImV4cCI6MjA2NzMyNDQwN30.6rmS5bhQO49DhTXFCMWtdtDSH_WOvKOxiT__1Hvwkr0';
 
 // Creamos el cliente de Supabase y lo hacemos disponible globalmente
